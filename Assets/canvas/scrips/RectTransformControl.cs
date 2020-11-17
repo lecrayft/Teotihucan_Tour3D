c8a4ef72255e4b0e87b84bdea6101c67ad05b470
Lecrayft <noelsanz93@gmail.com>
2020-11-17 16:12:12 -0600

    Primer commit

---
+++ Assets/canvas/scrips/RectTransformControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectTransformControl : MonoBehaviour
+{
+    public RectTransform scroll;
+    private void Update()
+    {
+        //////////// Vertical   ///////////////////////// 1
+
+        if (Input.GetKeyDown("1"))
+        {// if input 1
+
+            scroll.offsetMin = new Vector2(0, 0);
+            scroll.offsetMin = new Vector2(1, 710);
+
+        } // if input 1
+
+        //////////// Vertical   /////////////////////////  1
+        
+
+        //////////// Vertical   ///////////////////////// 2
+
+        if (Input.GetKeyDown("2"))
+        {// if input 2
+
+            scroll.offsetMin = new Vector2(1, -710);
+            scroll.offsetMin = new Vector2(0, 0);
+
+        } // if input 2
+
+        //////////// Vertical   ///////////////////////// 2
+
+        //////////// horizontal   /////////////////////////   3
+
+        if (Input.GetKeyDown("3"))
+        {// if input 3
+
+            scroll.offsetMin = new Vector2(0, 0);
+            scroll.offsetMin = new Vector2(710, 1);
+
+        } // if input 3
+
+        //////////// horizontal   /////////////////////////   3
+
+        //////////// horizontal   /////////////////////////  4
+
+        if (Input.GetKeyDown("4"))
+        {// if input 4
+
+            scroll.offsetMin = new Vector2(-710, 1);
+            scroll.offsetMin = new Vector2(0, 0);
+
+        } // if input 4
+
+        //////////// horizontal   /////////////////////////  4
+
+    }
+
+
+
+
+}
---
+++ Assets/canvas/scrips/Seleccionar_main.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Seleccionar_main : MonoBehaviour
+{
+
+    public GameObject Ventana_main_des; //VentanaVideoSol; // para las ventanas
+
+    Animation AMenu_des;                 //piramide_sol;  // para las animaciones
+
+    bool VisibleVentana_main_des;  //VisibleVentanaVideoSol = false; // identifica si la ventana esta visible o no 
+  
+    
+    
+    
+                    
+    void Start()
+    {
+        AMenu_des = Ventana_main_des.GetComponent<Animation>();  // piramide_sol = VentanaVideoSol.GetComponent<Animation>(); // iniciamos las variables a ventan con los componentes animation
+
+    }
+public void VentanaPulsada()
+{// inicio public void VentanaPulsada
+
+        if (VisibleVentana_main_des == false)
+        { // incio de if (visibleVentana ==false)
+
+            AMenu_des["AMenu_des"].speed = 1; // si es falso // nombre de La Animacion  
+            AMenu_des.Play(); // se ejecuta la ventana 
+            VisibleVentana_main_des = true; // se cambia a verdadero 
+
+        }// fin del if (visibleVentana ==false)
+
+   
+
+      
+
+
+    }// fin Public void ventana Pulsada 
+
+    public void VentanaNopulsada()
+{// public void ventana no pulsada
+ string NombreBoton = EventSystem.current.currentSelectedGameObject.name;
+
+
+    
+
+        if (NombreBoton != "AMenu_des" & VisibleVentana_main_des == true)
+        {// if de ventana_deslizante
+            AMenu_des["AMenu_des"].speed = -1;
+            AMenu_des["AMenu_des"].time = AMenu_des["AMenu_des"].length;
+            AMenu_des.Play();
+            VisibleVentana_main_des = false;
+
+        }// fin del if info sol
+
+       
+
+    }// fin del public void ventana no pulsada 
+}
